43965c0486efa17a3d1cecb4ff9922cdb4a69d11
Hailey Gaylor and Tom McElhinny <example@example.com>
2019-05-15 20:33:15 -0700

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
